009727eced92ad1ec12f8a3f41e25a8651bc528e
Luis Marin <luismgval@gmail.com>
2021-05-05 16:08:27 +0200

    Img perfil navBar

--- app/src/app/views/nav-bar/nav-bar.component.ts
+++ app/src/app/views/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,10 +10,16 @@ import { AuthService } from '@auth0/auth0-angular';
 export class NavBarComponent implements OnInit {
 
   login:boolean= false
+  perfilPicture:Observable<any> = new Observable()
 
   constructor(public auth: AuthService) {
     auth.isAuthenticated$.subscribe(data => this.login = data) //hide or show when we are login
     console.log("login",this.login);
+
+    this.auth.user$.subscribe((data:any)=>{
+     this.perfilPicture=data.picture;
+     
+    })
     
     
 
